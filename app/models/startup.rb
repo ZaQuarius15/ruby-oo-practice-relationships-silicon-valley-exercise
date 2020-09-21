c5e68be9339603fea83d0b013c749fe136f8d249
ZaQuarius15 <jzcarlson014@gmail.com>
2020-09-21 17:51:45 -0500

    Initial (full) commit

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,88 @@
 class Startup
 
+    @@all = []
+
+    attr_accessor :name
+    attr_reader :founder, :domain
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(name, domain)
+        @name = name
+        @domain = domain
+    end
+
+    def sign_contract(venture_capitalist, type, investment)
+        FundingRound.new(self, venture_capitalist, type, investment)
+    end
+
+    def num_funding_rounds
+        count = 0
+        FundingRound.all.each do |fund|
+            if fund.startup == self
+                count += 1
+            end
+        end
+        count
+    end
+
+    def total_funds
+        total = 0
+        FundingRound.all.each do |fund|
+            if fund.startup == self
+                total += fund.investment
+            end
+        end
+        total
+    end
+
+    def investors
+        investors = []
+        FundingRound.all.each do |fund|
+            if fund.startup == self
+                investors << fund.venture_capitalist
+            end
+        end
+        investors.uniq
+    end 
+
+    def big_investors
+        big_investors = []
+        FundingRound.all.each do |fund|
+            if VentureCapitalist.tres_commas_club.include?(fund.venture_capitalist)
+                if fund.startup == self
+                    big_investors << fund.venture_capitalist
+                end
+            end
+        end
+        big_investors.uniq
+    end
+
+
+    private
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder)
+        all.find do |startup|
+            startup.founder == founder
+        end
+    end
+
+    def self.domains
+        all.each_with_object([]) do |startup, final|
+            final << startup.domain
+        end
+    end
+
+    
+
+
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,71 @@
 class VentureCapitalist
 
+    @@all = []
+
+    attr_accessor :name, :total_worth
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+
+    def self.all
+        @@all
+    end
+
+
+    def self.tres_commas_club
+        @@all.each_with_object([]) do |ven, final|
+           if ven.total_worth >= 1000000000
+                final << ven
+           end
+        end
+    end
+    
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        funding_rounds = []
+        FundingRound.all.each do |fund|
+            if fund.venture_capitalist == self
+                funding_rounds << fund
+            end
+        end
+        funding_rounds
+    end 
+
+    def portfolio
+        startups = []
+        self.funding_rounds.each do |fund|
+            startups << fund.startup
+        end
+        startups.uniq
+    end 
+
+    def biggest_investment
+        biggest_investment = nil
+        self.funding_rounds.each do |fund|
+            if biggest_investment == nil
+                biggest_investment = fund
+            elsif fund.investment > biggest_investment.investment
+                biggest_investment = fund
+            end
+        end
+        biggest_investment
+    end
+
+    def invested(domain)
+        total_invested = 0
+        self.funding_rounds.each do |fund|
+            if fund.startup.domain == domain
+                total_invested += fund.investment
+            end
+        end
+        total_invested
+    end
+
 end
